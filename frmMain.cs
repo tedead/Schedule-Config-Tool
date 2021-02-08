c4ad16b45e00f8c86f7e76f4056c520f0363a3bd
tedead <34945023+tedead@users.noreply.github.com>
2021-02-08 11:43:17 -0600

    Add files via upload

---
+++ frmMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Schedule_Config_Tool
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            String test = "";
+            DataSet dataset = new DataSet();
+            dataset.ReadXml("schedule.xml");
+            foreach (DataRow r in dataset.Tables["Company"].Rows)
+            {
+                test = r["GPCompanyID"].ToString();
+
+                int a = 0;
+            }
+        }
+    }
+}
